e23cfdfacabf88efaf6c395b639dd7b345d579d8
Edib H <edib.hafizovic@gmail.com>
2024-10-29 15:36:15 +0100

    Implement db operations

--- database.py
+++ database.py
@@ -13,7 +13,7 @@ class Database:
 
     def _create_table(self):
         query = """
-            CREATE TABLE IF NOT EXISTS contacts(
+            CREATE TABLE IF NOT EXISTS notes(
                 id INTEGER PRIMARY KEY,
                 subject TEXT,
                 text TEXT
@@ -25,4 +25,27 @@ class Database:
         result = self.cursor.execute(query, [*query_args])
         self.db.commit()
         return result
+
+    def get_all_notes(self):
+        result = self._run_query("SELECT * FROM notes;")
+        return result.fetchall()
+
+    def get_last_note(self):
+        result = self._run_query(
+            "SELECT * FROM notes ORDER BY id DESC LIMIT 1;"
+        )
+        return result.fetchone()
+
+    def add_new_note(self, note):
+        self._run_query(
+            "INSERT INTO notes VALUES (NULL, ?, ?);", *note,
+        )
+
+    def delete_note(self, note_id):
+        self._run_query(
+            "DELETE FROM notes WHERE id=(?);", note_id,
+        )
+
+    def clear_all_notes(self):
+        self._run_query("DELETE FROM notes;")
 # End-of-file (EOF)
